257af6d3105ceabc102546494f623f47926dbf3f
sevan7 <32720226+sevan7@users.noreply.github.com>
2017-10-18 12:38:04 +0330

    Simple Calculator

---
+++ script.js
@@ -0,0 +1,26 @@
+function sum () {
+    let first = document.getElementById('firstone').value;
+    let second = document.getElementById('secondone').value;
+    z= +first + +second;
+    return(alert(z));  
+ }
+ function mines() {
+    let first = document.getElementById('firstone').value;
+    let second = document.getElementById('secondone').value;
+    z= +first - +second;
+    return(alert(z));
+ }
+ function multi() {
+    let first = document.getElementById('firstone').value;
+    let second = document.getElementById('secondone').value;
+    z= +first * +second;
+    return(alert(z));
+}
+function div() {
+    let first = document.getElementById('firstone').value;
+    let second = document.getElementById('secondone').value;
+    z= +first / +second;
+    return(alert(z));
+
+}
+
